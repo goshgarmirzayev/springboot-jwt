1fc3254b5ccb71463b90eed0be851165ac8cc741
Mahammad <mahammad.mammadovit@gmail.com>
2018-10-25 19:27:19 +0400

    changes

--- src/main/java/com/nouhoun/springboot/jwt/integration/service/GenericService.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/service/GenericService.java
@@ -5,10 +5,10 @@ import com.nouhoun.springboot.jwt.integration.domain.User;
 
 import java.util.List;
 
-/**
- * Created by nydiarra on 06/05/17.
- */
+
+
 public interface GenericService {
+    
     User findByUsername(String username);
 
     List<User> findAllUsers();
